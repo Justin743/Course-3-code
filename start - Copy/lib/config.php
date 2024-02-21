60c36e09c2c2a2191bf42f2ea3e6ab02b5dffb01
Justin743 <B00155152@mytudublin.ie>
2024-02-21 09:07:01 +0000

    Added completed code

---
+++ start - Copy/lib/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$configVars = array(
+    'database_dsn'  => 'mysql:dbname=air_pup;host=localhost',
+    'database_user' => 'root',
+    'database_pass' => 'Juoz9988/',
+);
+
+return $configVars;
